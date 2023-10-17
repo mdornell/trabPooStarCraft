bb4fdba20c7eb865b2cb51e56d48536264ac96c2
Marco Antônio de Paula Dornel <87149031+mdornell@users.noreply.github.com>
2023-10-17 12:56:12 -0300

    Delete TrabPOO directory

--- TrabPOO/src/characters/abstracts/ProtossMechanic.java
+++
@@ -1,9 +0,0 @@
-package characters.abstracts;
-
-public abstract class ProtossMechanic extends Mechanic{
-
-    public ProtossMechanic(String nome, int idade, double peso) {
-        super(nome, idade, peso);
-    }
-    
-}
--- TrabPOO/src/characters/abstracts/TerransOrganic.java
+++
@@ -1,9 +0,0 @@
-package characters.abstracts;
-
-public abstract class TerransOrganic extends Organic{
-
-    public TerransOrganic(String nome, int idade, double peso) {
-        super(nome, idade, peso);
-    }
-    
-}
