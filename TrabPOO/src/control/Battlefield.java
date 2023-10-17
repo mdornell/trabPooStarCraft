7ed8783c6a495638ecb936d3986df94539a68c85
RonanJardim <129337165+RonanJardim@users.noreply.github.com>
2023-10-17 14:31:09 -0300

    jttnhtytj

--- TrabPOO/src/control/Battlefield.java
+++ TrabPOO/src/control/Battlefield.java
@@ -5,23 +5,40 @@ import java.util.LinkedList;
 // import characters.abstracts.Guerreiro;
 
 public class Battlefield {
-    
-    public int Random{
-        int num = (int)(Math.Random());
+
+    public int numAleatorio(){
+        int num = (int)((Math.Random()*50)+1);
+        
+        return num;
     }
 
-    public void batalha(LinkedList<Guerreiro> lado1, LinkedList<Guerreiro> lado2){
+    public void batalha(LinkedList<Guerreiro> lado1, LinkedList<Guerreiro> lado2, int num){
+        if(!lado1.isEmpty() && !lado2.isEmpty()){
 
+            Guerreiro guerreiro1 = lado1.get(0);
+            Guerreiro guerreiro2 = lado2.get(0);
+
+            atacarPrimeiro(num, guerreiro1, guerreiro2, lado1, lado2);
+            //atacarFila(){}
+            //moverFinalDaFila(){}
+        }
     }
 
-
-    if(num < 0,5){
+    private int atacarPrimeiro(int num, Guerreiro primeiroAtacar, Guerreiro segundoAtacar, LinkedList<Guerreiro> lado1, LinkedList<Guerreiro> lado2){
+        if(num < 25){
         // Se o número gerado for menor, o guerreiro da fila é o primeiro a atacar
 
-    }else{
+        }else{
         //Se não, o guerreiro da segunda fila é o primeiro a atacar
+        }       
     }
 
-
-
+//    public void moverFinalDaFila(){
+     
+//     }
+    public void atacarFila(){
+        if(primeiroAtacar.getEnergia() > 0){
+            primeiroAtacar.atacar(segundoAtacar, lado1, lado2)
+        }
+    }
 }
