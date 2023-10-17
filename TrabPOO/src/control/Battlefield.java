9a2605296fe99f2aa6da6a423cda343607090709
RonanJardim <129337165+RonanJardim@users.noreply.github.com>
2023-10-17 13:49:19 -0300

    aula

--- TrabPOO/src/control/Battlefield.java
+++ TrabPOO/src/control/Battlefield.java
@@ -1,45 +1,27 @@
 package control;
-
-// import java.util.LinkedList;
+import java.lang.Math.*;
+import java.util.LinkedList;
 
 // import characters.abstracts.Guerreiro;
 
 public class Battlefield {
     
-    // public void batalha(LinkedList<Guerreiro> LadoTp,LinkedList<Guerreiro> LadoZn){
-    //     if(!LadoTp.isEmpty() && !LadoZn.isEmpty()){
-    //         int numRandom = 1 + ((int)(Math.random()*50));
+    public int Random{
+        int num = (int)(Math.Random());
+    }
 
-    //         Guerreiro gTp = LadoTp.get(0);
-    //         Guerreiro gZn = LadoZn.get(0);
-            
-    //         quemAtaca(numRandom, gTp, gZn, LadoTp, LadoZn);
+    public void batalha(LinkedList<Guerreiro> lado1, LinkedList<Guerreiro> lado2){
 
-    //     }
-    // }
+    }
 
-    // public void quemAtaca(int orderAttack,Guerreiro gTp, Guerreiro gZn,LinkedList<Guerreiro> LadoTp,LinkedList<Guerreiro> LadoZn){
-    //     boolean  permission;
-    //     if(orderAttack > 25){
-    //         permission = ataque(gTp,gZn,LadoTp,LadoZn);
-    //         if(permission){
-    //             ataque(gZn, gTp, LadoZn, LadoTp);
-    //         }
-    //     }else{
-    //         ataque(gZn, gTp, LadoZn, LadoTp);
-    //         ataque(gTp,gZn,LadoTp,LadoZn);
-    //     }
 
-    // }
+    if(num < 0,5){
+        // Se o número gerado for menor, o guerreiro da fila é o primeiro a atacar
 
-    // public boolean ataque(Guerreiro gAttack,Guerreiro gDamage,LinkedList<Guerreiro> lAttack,LinkedList<Guerreiro> lDamage){
-    //     // boolean permission = true;
-    //     if(gAttack.getEnergia() > 0){
-    //         // permission = gAttack.atacar(gDamage, lAttack, lDamage);
-    //     }
-    //     return true;
-    // }
+    }else{
+        //Se não, o guerreiro da segunda fila é o primeiro a atacar
+    }
 
-    
+
 
 }
